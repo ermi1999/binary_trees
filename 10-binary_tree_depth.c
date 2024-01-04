4ff599d9c021dba8189b698144d00e3b1b19edb8
Ermiyas <ermiyasabiye1999@gmail.com>
2024-01-05 00:47:44 +0300

    fixed 9 binary height

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,13 +1,20 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth - measures the depth of a binary tree
- * @tree: tree to measure the depth
- *
- * Return: depth of the tree
- *         0 if tree is NULL
+ * binary_tree_depth - measures the depth of a node in a binary tree.
+ * @tree: is a pointer to the node to measure the depth.
+ * Return: The depth of the node.
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+	size_t depth = 0;
+
+	if (tree == NULL)
+		return(0);
+	while (tree->parent)
+	{
+		tree = tree->parent;
+		depth++;
+	}
+	return (depth);
 }
