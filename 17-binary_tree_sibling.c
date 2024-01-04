fe41cab3e3edb8538f40cf81fb7cd58edeb9bfeb
Ermiyas <ermiyasabiye1999@gmail.com>
2024-01-05 01:59:49 +0300

    fixed sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_sibling - finds the sibling of a node.
  * @node: is a pointer to the node to find the sibling.
- * @Return: a pointer to the sibling of the node.
+ * Return: a pointer to the sibling of the node.
  */
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
