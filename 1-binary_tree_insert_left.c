361b92210531269a6b7504d76c86b6cab4f7afc6
Ermiyas <ermiyasabiye1999@gmail.com>
2024-01-04 23:19:05 +0300

    is root & preorder

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,19 +10,19 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-binary_tree_t *newNode;
-if (parent == NULL)
-return (NULL);
-newNode = malloc(sizeof(binary_tree_t));
-if (newNode == NULL)
-return (NULL);
-newNode->n = value;
-newNode->parent = parent;
-newNode->left = parent->left;
-newNode->right = NULL;
-parent->left = newNode;
-if (newNode->left)
-newNode->left->parent = newNode;
+	binary_tree_t *newNode;
 
-return (newNode);
+	if (parent == NULL)
+		return (NULL);
+	newNode = malloc(sizeof(binary_tree_t));
+	if (newNode == NULL)
+		return (NULL);
+	newNode->n = value;
+	newNode->parent = parent;
+	newNode->left = parent->left;
+	newNode->right = NULL;
+	parent->left = newNode;
+	if (newNode->left)
+		newNode->left->parent = newNode;
+	return (newNode);
 }
