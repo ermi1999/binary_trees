6b53115400d4617f12cb9677147800906e20d4c4
tgairwoman <tgjehovah@gmail.com>
2024-01-04 22:34:01 +0300

    Insert right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,22 +10,19 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    if (parent == NULL)
-        return NULL;
+binary_tree_t *new;
+if (parent == NULL)
+return (NULL);
+new = malloc(sizeof(binary_tree_t));
+if (new == NULL)
+return (NULL);
+new->n = value;
+new->parent = parent;
+new->left = NULL;
+new->right = parent->right;
+parent->right = new;
+if (new->right)
+new->right->parent = new;
 
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-        return NULL;
-
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = parent->right;
-
-    if (parent->right != NULL)
-        parent->right->parent = new_node;
-
-    parent->right = new_node;
-
-    return new_node;
+return (new);
 }
