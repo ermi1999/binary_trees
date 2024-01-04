b27c3f942e2d7207eda4be6bb60df9d44336a1f4
tgairwoman <tgjehovah@gmail.com>
2024-01-05 02:07:58 +0300

    uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -3,4 +3,14 @@
 /**
  * binary_tree_uncle - finds the uncle of a node
  * @node: is a pointer to the node to find the uncle
- * Return: a pointer to the uncle node or null
+ * Return: a pointer to the uncle node or null.
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *parent;
+
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+
