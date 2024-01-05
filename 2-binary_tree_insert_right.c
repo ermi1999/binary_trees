4f88d9bff282068d92763fa631e5d895c46bbbb5
Ermiyas <ermiyasabiye1999@gmail.com>
2024-01-05 17:40:11 +0300

    fixed insert right styling

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,13 +11,13 @@
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
-	
+
 	if (parent == NULL)
 		return (NULL);
 	new = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
 		return (NULL);
-	
+
 	new->n = value;
 	new->parent = parent;
 	new->left = NULL;
